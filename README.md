8a9746b1855a1164caa5385cd8a74d3597da5009
Ed Dao <ed@intellogic.com>
2025-09-08 12:53:17 -0700

    add keyring support

--- README.md
+++ README.md
@@ -1,14 +1,27 @@
 # Loom
 
 Weaves the fabric of information and time into one reality.
-The Loom capability will grow as my need grows.  My tenet is do not over implement until needs arise.
+
+## North Star
+A tapestry to which ideas are carried into reality swiftly, safely, and simply.  The two most important concept of such reality is information and time.
+
+## Goal
+- Lower mental load of coding
+- Create friction for writing unsafe code for two of the hardest concept information management and time
+    - a developer can always write themselves into a deep pit.  Let do our best to make that harder
+- Nice simple code.
+    - willing to sacrifice capability for simpler code.  For example we might forever support only ONE database (mongodb). 
+    - Feel free to clone to another Loom to support another database paradigm.  Since most likely the will complicate our code and increase the mental load of two different information management concept
+
+## Non Goal / Anti Goal
+This is not intended as a swiss army knife of coding framework.  The Loom capability will grow as needs grow.  Our tenet is to not over implement until needs arise.
 
 ## Information
 
 Initial limitation for simplicity:
 - support only local and remote mongodb database
-- secret are access through google-cloud-secret-manager
-    - this can be easily extended to other cloud provider (Not yet implemented)
+- cloud secret are access through google-cloud-secret-manager
+    - this can be easily extended to other cloud secret manager (Not yet implemented)
 
 
 ## Time
--- loom/info/universal.py
+++ loom/info/universal.py
@@ -1,38 +1,65 @@
+from enum import Enum
+import getpass
 import os
 import threading
 from typing import Optional
 from bson import ObjectId
 from google.cloud import secretmanager
+import keyring
 import pymongo
 
 LOCAL_MONGODB_CLIENT_URI = "mongodb://localhost:27017/"
 ZERO_ID = ObjectId("000000000000000000000000")
 
 
+class SecretManager(Enum):
+    GCS_SECRET_MANAGER = "gcs_secret_manager"
+    LOCAL_KEYRING = "keyring"
+
+
 def access_secret(
-    secret_id: str, project_id: Optional[str] = None, version_id: str = "latest"
+    secret_id: str, 
+    source_id: Optional[str] = None, 
+    version_id: str = "latest", 
+    manager: SecretManager = SecretManager.GCS_SECRET_MANAGER
 ) -> str:
     """
-    Accesses a secret from Google Cloud Secret Manager.
+    Accesses a secret from a specified secret manager.
 
     Args:
-        project_id (str, optional): The ID of the Google Cloud project. Defaults
-            to the value of the `GOOGLE_CLOUD_PROJECT_NUM` environment variable.
         secret_id (str): The ID of the secret.
-        version_id (str, optional): The version of the secret. Defaults to `"latest"`.
+        source_id (str, optional): The source identifier. For GCS, this is the
+            project ID (defaults to `GOOGLE_CLOUD_PROJECT_NUM` env var). For
+            local keyring, this is the username (defaults to `LOCAL_KEYRING_USERNAME`
+            env var or the current user).
+        version_id (str, optional): The version of the secret (for GCS).
+            Defaults to `"latest"`.
+        manager (SecretManager, optional): The secret manager to use.
+            Defaults to `SecretManager.GCS_SECRET_MANAGER`.
 
     Returns:
         str: The secret payload.
     """
 
-    project_num = project_id or os.getenv("GOOGLE_CLOUD_PROJECT_NUM")
-    if (project_num is None) or (len(project_num) == 0):
-        raise Exception("GOOGLE_CLOUD_PROJECT_NUM is not set")
+    match manager:
+        case SecretManager.GCS_SECRET_MANAGER:
+            project_num = source_id or os.getenv("GOOGLE_CLOUD_PROJECT_NUM")
+            if (project_num is None) or (len(project_num) == 0):
+                raise Exception("GOOGLE_CLOUD_PROJECT_NUM is not set")
 
-    client = secretmanager.SecretManagerServiceClient()
-    name = f"projects/{project_num}/secrets/{secret_id}/versions/{version_id}"
-    response = client.access_secret_version(request={"name": name})
-    return response.payload.data.decode("utf-8")
+            client = secretmanager.SecretManagerServiceClient()
+            name = f"projects/{project_num}/secrets/{secret_id}/versions/{version_id}"
+            response = client.access_secret_version(request={"name": name})
+            return response.payload.data.decode("utf-8")
+        case SecretManager.LOCAL_KEYRING:
+            username = source_id or os.getenv("LOCAL_KEYRING_USERNAME") or getpass.getuser()
+
+            ret = keyring.get_password(secret_id, username)
+            if ret is None:
+                raise Exception("Secret not found in keyring")
+            return ret
+
+    raise Exception("Unknown secret manager")
 
 
 # TLV Thread Local Variable
