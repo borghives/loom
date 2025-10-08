f4f300ed8a448317c10c5d6ac4e977a885847578
Ed Dao <ed@intellogic.com>
2025-10-08 10:58:30 -0700

    add load dotenv

--- loom/info/universal.py
+++ loom/info/universal.py
@@ -8,6 +8,11 @@ from google.cloud import secretmanager
 import keyring
 import pymongo
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
 LOCAL_MONGODB_CLIENT_URI = "mongodb://localhost:27017/"
 ZERO_ID = ObjectId("000000000000000000000000")
 
