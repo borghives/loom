d5ad70d43cfeb7e543c1cdb80ab7e2d7194f9e67
Ed Dao <ed@intellogic.com>
2026-03-22 22:51:05 -0700

    update

--- loom/info/db_driver.py
+++ loom/info/db_driver.py
@@ -99,20 +99,14 @@ class MongoDbModelDriver:
 
         name = self.get_db_collection_name()
 
-        try:
-            db.create_collection(name, check_exists=False, **kvargs)
-        except errors.CollectionInvalid:
-            pass
+        db.create_collection(name, **kvargs)
 
     async def create_collection_async(self, **kvargs):
         db = self.get_db(with_async=True)
         assert isinstance(db, AsyncDatabase)
         name = self.get_db_collection_name()
 
-        try:
-            await db.create_collection(name, check_exists=False, **kvargs)
-        except errors.CollectionInvalid:
-            pass
+        await db.create_collection(name, **kvargs)
 
     def create_index(self):
         indexes = self.index
